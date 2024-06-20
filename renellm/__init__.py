117599f329c35b2d6b38630f974812504ec993c3
yasu52 <tsuguro4649@gmail.com>
2024-06-20 13:42:25 -0700

    Update ReNeLLM for packaging

---
+++ renellm/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+
+from .data_utils import data_reader
+from .prompt_rewrite_utils import shortenSentence, misrewriteSentence, changeOrder, addChar, languageMix, styleChange
+from .scenario_nest_utils import SCENARIOS
+from .harmful_classification_utils import harmful_classification
